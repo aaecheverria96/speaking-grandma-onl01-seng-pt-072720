5a91fb93139ef838682e12eafa09354d018d5214
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-31 17:26:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 def speak_to_grandma (phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "Hi Nana, how are you?" 
+    return "HUH?! SPEAK UP, SONNY!" 
+elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
